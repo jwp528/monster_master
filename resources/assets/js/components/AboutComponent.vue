c59ae50f6428778097fb8f4dc52c23663ad83aa2
Josh <Parsons>
2019-04-18 22:27:52 -0400

    finalized details in about page

--- resources/assets/js/components/AboutComponent.vue
+++ resources/assets/js/components/AboutComponent.vue
@@ -1,59 +1,67 @@
 <template>
-    <div class="col-12 bg-light py-3">
-        <div class="col-lg-8 col-sm-12 mx-auto">
-            <h1 class="text-center mb-5">Monster Master</h1>
+  <div class="col-12 bg-light py-3">
+    <div class="col-lg-8 col-sm-12 mx-auto">
+      <h1 class="text-center mb-5">Monster Master</h1>
 
-            <h4 class="text-muted">Base Functionality</h4>
-            <p>
-                After taking the default application from the tutorial. I sat down, looked over how the application was put together,
-                and the different components that went into it; and started thinking about what I was going to do on top of this.
-            </p>
+      <h4 class="text-muted">Base Functionality</h4>
+      <p>
+        After taking the default application from the tutorial. I sat down, looked over how the application was put together,
+        and the different components that went into it; and started thinking about what I was going to do on top of this.
+      </p>
 
-            <h4 class="text-muted mt-5">Deciding not to do the traditional "Todo List" app.</h4>
-            <p>
-                The first thing that came to mind was to take the suggestion of the email I'd received stating
-                I could do another app within the already existing one, but that died when I looked at what I currently had in front of me
-                and realized it looked a bit like something you'd see in a Pokémon game.
-            </p>
-            <p>
-                I decided to make a small battle system out of it. I started with the design, adding bootstrap to the project
-                so I could take care of things like the buttons, controls, placement, and to create a look that was still good even on mobile.
-            </p>
+      <h4 class="text-muted mt-5">Deciding not to do the traditional "Todo List" app.</h4>
+      <p>
+        The first thing that came to mind was to take the suggestion of the email I'd received stating
+        I could do another app within the already existing one, but that died when I looked at what I currently had in front of me
+        and realized it looked a bit like something you'd see in a Pokémon game.
+      </p>
+      <p>
+        I decided to make a small battle system out of it. I started with the design, adding bootstrap to the project
+        so I could take care of things like the buttons, controls, placement, and to create a look that was still good even on mobile.
+      </p>
 
-            <h4 class="text-muted mt-5">Creating the Battle System</h4>
-            <p>
-                After deciding on the look I sat down and modelled out how the battle system was going to work. I added a new Migration
-                to the project to add the description, HP, Attack, and Defense stats. and used the already implemented Faker object to generate
-                numbers between what I thought was a reasonable range of values.
-            </p>
-            <p>
-                I then set to displaying those new properties, adding the parameters to the object, assigning them to their proper location.
-                and even adding new computed properties to keep track of the currentHP of a given monster, and whether that monster was set
-                as the active monster. I eventually got to the point where I used bootstraps progress bars coupled with the current HP value
-                to create the animated health bar shown in the application.
-            </p>
-            <p>
-                Once the elements were in place it was time to add the functionality. I added the setActive and attack buttons.
-                I set the setActive button to be visible dependent on if the monster was the active monster and the attack button was a simiar check
-                with a check to make sure an active monster was set at all as well.
-            </p>
+      <h4 class="text-muted mt-5">Creating the Battle System</h4>
+      <p>
+        After deciding on the look I sat down and modelled out how the battle system was going to work. I added a new Migration
+        to the project to add the description, HP, Attack, and Defense stats. and used the already implemented Faker object to generate
+        numbers between what I thought was a reasonable range of values.
+      </p>
+      <p>
+        I then set to displaying those new properties, adding the parameters to the object, assigning them to their proper location.
+        and even adding new computed properties to keep track of the currentHP of a given monster, and whether that monster was set
+        as the active monster. I eventually got to the point where I used bootstraps progress bars coupled with the current HP value
+        to create the animated health bar shown in the application.
+      </p>
+      <p>
+        Once the elements were in place it was time to add the functionality. I added the setActive and attack buttons.
+        I set the setActive button to be visible dependent on if the monster was the active monster and the attack button was a simiar check
+        with a check to make sure an active monster was set at all as well.
+      </p>
 
-            <h4 class="text-muted mt-5">Further Expansion</h4>
-            <p>
-                After I'd completed the application I decided it needed something more. I'd established my ability to alter the database
-                and use migrations, complete basic CRUD functionality and further the client functionality, but I felt it was lacking
-                something. So I included the vue router as part of the application and built the routes to the application and this
-                write up to demonstrate that I can implement functionality from scratch as well.
-            </p>
+      <h4 class="text-muted mt-5">Testing</h4>
+      <p>
+        When testing the application I realized that updating the currentHP of the monsters on each attack was resulting in
+        too many requests being sent to the api. So I opted to switch it out so that when a monster is defeated, the one who
+        defeated it has it's hp recorded.
+      </p>
 
-            <h4 class="text-muted mt-5">The Main Development Process</h4>
-            <p>
-                I started this application with a very basic understanding of laravel, and never having used the vue-cli at all. My Process
-                while building out this application was to read and experiment with the laravel framework and discover how the application back
-                end worked best using the vue-cli on the front end.
-            </p>
-        </div>
+      <h4 class="text-muted mt-5">Further Expansion</h4>
+      <p>
+        After I'd completed the application I decided it needed something more. I'd established my ability to alter the database
+        and use migrations, complete basic CRUD functionality and further the client functionality, but I felt it was lacking
+        something. So I included the vue router as part of the application and built the routes to the application and this
+        write up to demonstrate that I can implement functionality from scratch as well.
+      </p>
+
+      <h4 class="text-muted mt-5">The Main Development Process</h4>
+      <p>
+        I started this application with a very basic understanding of laravel, and never having used the vue-cli at all. My Process
+        while building out this application was to read and experiment with the laravel framework and discover how the application back
+        end worked best using the vue-cli on the front end.
+      </p>
+      <p>I learned a lot about dependency management, the node package manager, and webpack as well.</p>
     </div>
+  </div>
 </template>
 
 export default{
--- resources/assets/js/components/CrudsComponent.vue
+++ resources/assets/js/components/CrudsComponent.vue
@@ -1,150 +1,145 @@
 <template>
-    <div id="app">
-        <div class="col-12 mx-auto">
-            <div class="heading">
-                <h1>Cruds <button class="btn btn-primary" @click="create()">Add</button></h1>
-            </div>
-            <crud-component
-                    v-for="crud in cruds"
-                    v-bind="crud"
-                    :key="crud.id"
-                    @update="update"
-                    @delete="del"
-                    @setActive="setActive"
-                    @attack="attack"
-            ></crud-component>
-        </div>
+  <div id="app">
+    <div class="col-12 mx-auto">
+      <div class="heading">
+        <h1>
+          Cruds
+          <button class="btn btn-primary" @click="create()">Add</button>
+        </h1>
+      </div>
+      <crud-component
+        v-for="crud in cruds"
+        v-bind="crud"
+        :key="crud.id"
+        @update="update"
+        @delete="del"
+        @setActive="setActive"
+        @attack="attack"
+      ></crud-component>
     </div>
+  </div>
 </template>
 <script>
-  function Crud({ id, color, name, description, hp, currentHp, atk, def}) {
-    this.id = id;
-    this.color = color;
-    this.name = name;
-    this.description = description;
-    this.hp = hp;
-    this.currentHp = currentHp;
-    this.atk = atk;
-    this.def = def;
-  }//end Crud
+function Crud({ id, color, name, description, hp, currentHp, atk, def }) {
+  this.id = id;
+  this.color = color;
+  this.name = name;
+  this.description = description;
+  this.hp = hp;
+  this.currentHp = currentHp;
+  this.atk = atk;
+  this.def = def;
+} //end Crud
 
-  import CrudComponent from './Crud.vue';
+import CrudComponent from "./Crud.vue";
 
-  export default {
-    data() {
-      return {
-        cruds: [],
-        activeCrud: "",
-        mute: false
-      }
+export default {
+  data() {
+    return {
+      cruds: [],
+      activeCrud: "",
+      mute: false
+    };
+  },
+  methods: {
+    create() {
+      this.mute = true;
+      window.axios.get("/api/cruds/create").then(({ data }) => {
+        this.cruds.push(new Crud(data));
+        this.mute = false;
+      });
     },
-    methods: {
-      create() {
-        this.mute = true;
-        window.axios.get('/api/cruds/create').then(({ data }) => {
-          this.cruds.push(new Crud(data));
-          this.mute = false;
+    read() {
+      this.mute = true;
+      window.axios.get("/api/cruds").then(({ data }) => {
+        data.forEach(crud => {
+          this.cruds.push(new Crud(crud));
         });
-      },
-      read() {
-        this.mute = true;
-        window.axios.get('/api/cruds').then(({ data }) => {
-          data.forEach(crud => {
-            this.cruds.push(new Crud(crud));
-          });
-          this.mute = false;
-        });
-      },
-      update(id, color, hp) {
-        this.mute = true;
-        window.axios.put(`/api/cruds/${id}`, { color, hp }).then(() => {
-          this.cruds.find(crud => crud.id === id).color = color;
-          this.cruds.find(crud => crud.id === id).currentHp = hp;
-          this.mute = false;
-        });
-      },
-      del(id, newactive = '') {
-        this.mute = true;
-
-        window.axios.delete(`/api/cruds/${id}`).then(() => {
-          let index = this.cruds.findIndex(crud => crud.id === id);
-          this.cruds.splice(index, 1);
-
-          if(this.activeCrud === id)
-            this.setActive(newactive);
-
-          this.mute = false;
-        });//end axios call
-      },//end del
-      setActive(id){
-        console.log(`crud ${id} is being set to active`);
-        this.activeCrud = id;
-      },//end setActive
-      attack(target){
-        //the attacking crud goes first
-        //damage is calculated with the following formula
-        //damage = (target defense / active crud attack) * active crud attack
-        let defender = this.cruds.filter(crud => {
-          return crud.id === target;
-        });//end defender assign
-        defender = defender[0];
-
-        let attacker = this.cruds.filter(crud => {
-          return crud.id === this.activeCrud;
-        });//end attacker assign
-        attacker = attacker[0];
-
-        //attack the defending crud
-        let damage = Math.ceil((attacker.atk / defender.def) * attacker.atk);
-
-        if(defender.currentHp - damage <= 0){
-          defender.currentHp = 0;
-          this.del(defender.id);
-        }else{
-            defender.currentHp -= damage;
-            this.update(defender.id, defender.color, defender.currentHp);
-        }//end else
-
-        //the defending crud gets a counter attack
-        damage = Math.ceil((defender.atk / attacker.def) * defender.atk);
-
-        if(attacker.currentHp - damage <= 0){
-          attacker.currentHp = 0;
-          this.del(attacker.id, defender.id);
-        }else{
-            attacker.currentHp -= damage;
-            this.update(attacker.id, attacker.color, attacker.currentHp);
-        }//end else
-
-        for(var i = 0; i < this.cruds.length; ++i){
-
-          if(this.cruds[i].id === target)
-            this.cruds[i] = defender;
-
-          if(this.cruds[i].id == this.activeCrud)
-            this.cruds[i] = attacker;
-        }//end for
-      },//end attack
+        this.mute = false;
+      });
     },
-    watch: {
-      mute(val) {
-        document.getElementById('mute').className = val ? "on" : "";
-      }
+    update(id, color, hp) {
+      this.mute = true;
+      window.axios.put(`/api/cruds/${id}`, { color, hp }).then(() => {
+        this.cruds.find(crud => crud.id === id).color = color;
+        this.cruds.find(crud => crud.id === id).currentHp = hp;
+        this.mute = false;
+      });
     },
-    components: {
-      CrudComponent
-    },
-    created() {
-      this.read();
+    del(id, newactive = "") {
+      this.mute = true;
+
+      window.axios.delete(`/api/cruds/${id}`).then(() => {
+        let index = this.cruds.findIndex(crud => crud.id === id);
+        this.cruds.splice(index, 1);
+
+        if (this.activeCrud === id) this.setActive(newactive);
+
+        this.mute = false;
+      }); //end axios call
+    }, //end del
+    setActive(id) {
+      console.log(`crud ${id} is being set to active`);
+      this.activeCrud = id;
+    }, //end setActive
+    attack(target) {
+      //the attacking crud goes first
+      //damage is calculated with the following formula
+      //damage = (target defense / active crud attack) * active crud attack
+      let defender = this.cruds.filter(crud => {
+        return crud.id === target;
+      }); //end defender assign
+      defender = defender[0];
+
+      let attacker = this.cruds.filter(crud => {
+        return crud.id === this.activeCrud;
+      }); //end attacker assign
+      attacker = attacker[0];
+
+      //attack the defending crud
+      let damage = Math.ceil((attacker.atk / defender.def) * attacker.atk);
+
+      if (defender.currentHp - damage <= 0) {
+        defender.currentHp = 0;
+        this.del(defender.id);
+        this.update(attacker.id, attacker.color, attacker.currentHp);
+      } else defender.currentHp -= damage;
+
+      //the defending crud gets a counter attack
+      damage = Math.ceil((defender.atk / attacker.def) * defender.atk);
+
+      if (attacker.currentHp - damage <= 0) {
+        attacker.currentHp = 0;
+        this.del(attacker.id, defender.id);
+        this.update(defender.id, defender.color, defender.currentHp);
+      } else attacker.currentHp -= damage;
+
+      for (var i = 0; i < this.cruds.length; ++i) {
+        if (this.cruds[i].id === target) this.cruds[i] = defender;
+
+        if (this.cruds[i].id == this.activeCrud) this.cruds[i] = attacker;
+      } //end for
+    } //end attack
+  },
+  watch: {
+    mute(val) {
+      document.getElementById("mute").className = val ? "on" : "";
     }
+  },
+  components: {
+    CrudComponent
+  },
+  created() {
+    this.read();
   }
+};
 </script>
 <style>
-    #app {
-        margin-left: 1em;
-    }
+#app {
+  margin-left: 1em;
+}
 
-    .heading h1 {
-        margin-bottom: 0;
-    }
+.heading h1 {
+  margin-bottom: 0;
+}
 </style>
